985c4d7e18d817a1f8eaa582ee6a2abd9906ce61
User <user@macbook-pro-de-user-4.local>
2019-01-17 20:06:10 -0200

    Initial Commit

---
+++ terrainGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  terrainGenerator
+//
+//  Created by User on 17/01/19.
+//  Copyright © 2019 mickael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
